32e74659632df1f7f6e1bffdeeb1bf3d7285baa2
Renato Alves Pereira <renatorv@gmail.com>
2021-12-07 18:06:30 -0300

    44 minutos

--- usecase/process_transaction/process_transaction_test.go
+++ usecase/process_transaction/process_transaction_test.go
@@ -2,4 +2,23 @@ package processtransaction
 
 import "testing"
 
-func TestProcessTransaction_ExecuteInvalidCreditCard(t *testing.T) {}
+func TestProcessTransaction_ExecuteInvalidCreditCard(t *testing.T) {
+
+	input := TransactionDtoInput{
+		ID:                        "1",
+		AccountID:                 "1",
+		CreditCardNumber:          "40000000000000000",
+		CreditCardName:            "Renato Alves",
+		CreditCardExpirationMonth: 12,
+		CreditCardExpirationYear:  time.Now().Year(),
+		CreditCardCVV:             123,
+		Amount:                    200,
+	}
+
+	expectedOutPut := TransactionDtoOutPut{
+		ID:           "1",
+		Status:       "rejected",
+		ErrorMessage: "",
+	}
+
+}
